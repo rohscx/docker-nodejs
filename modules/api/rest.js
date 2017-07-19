9e075f70908261557a72bd76985d599849aa09da
rohscx <rohscx@gmail.com>
2017-07-19 13:36:38 -0400

    Update rest.js

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -1,28 +1,10 @@
-var httpRequest = require("http-request")
-var http = require("http")
+var request = require('request');
 module.exports = () => {
-var reqBody = {
-	param1: 'value1',
-	param2: 'value2'
-};
-
-// this serialization also does URL encoding so you won't have to
-reqBody = querystring.stringify(reqBody);
-
-http.post({
-	url: 'http://localhost/post',
-	reqBody: new Buffer(reqBody),
-	headers: {
-		// specify how to handle the request, http-request makes no assumptions
-		'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-	}
-}, 'post.bin', function (err, res) {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	
-	console.log(res.code, res.headers, res.file);
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
 });
   return (
     {anotherTestResponse: "Cats on everything from API REst"}
