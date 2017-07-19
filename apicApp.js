ab60f89d4b630d0dfc94b8d70805921af9b079a3
rohscx <R_E__g_@Macintosh.local>
2017-07-19 11:55:21 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -3,6 +3,3 @@ var apicTicket = require('./modules/apicTicket');
 var newTicket = apicTicket();
 
 console.log(newTicket.testResponse);
-console.log(newTicket.output);
-console.log(newTicket.ghostface);
-console.log(newTicket.test);
--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -2,10 +2,8 @@ var rest = require('./api/rest');
 module.exports = () => {
   var newRest = rest();
   var output = newRest.anotherTestResponse;
+  console.log("test");
   return (
-    {testResponse: "Cats on everything from Apic Ticket",
-      gostface: output
-    }
-    test
+    {testResponse: "Cats on everything from Apic Ticket"}
   );
 }
