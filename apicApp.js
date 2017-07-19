7823be675b550b86ef8f59b354e83aefc758db6a
rohscx <rohscx@gmail.com>
2017-07-19 16:49:45 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -1,7 +1,7 @@
 var restReqeust = require('./modules/apicTicket');
-var test = restReqeust.POST(function(err, result) {
-  if (err) handle(err);
-  console.log(result);
+var test = restReqeust.POST(function (err, data) {
+  if (err) return console.error(err)
+  console.log(data)
 });
   
 //console.log(test) // REST POST
