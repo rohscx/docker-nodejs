5d8af1803d5be2461089afd376e69a81b667bd07
rohscx <rohscx@gmail.com>
2017-07-19 15:15:37 -0400

    Update apicTicket.js

--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -1,14 +1,10 @@
 var rest = require('./api/rest');
 module.exports = () => {
-  var newRest = rest.POST();
-  var newRest2 = rest.GET();
-  var output = newRest;
-  var output2 = newRest2;
   return (
     {
       testResponse: "Cats on everything from Apic Ticket",
-      test:output,
-      test2:output2
+      test:rest.POST(),
+      test2:rest.GET()
     }
   );
 }
