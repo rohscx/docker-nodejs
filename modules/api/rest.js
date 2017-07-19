909024bc9cbd307242956636f6dc603b00c83a05
rohscx <rohscx@gmail.com>
2017-07-19 15:27:59 -0400

    Update rest.js

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var formData = require('form-data');
 
-module.exports = class Calculator {
+module.exports = class calculator {
   add(a,b) { return a + b }
   substract(a,b) { return a - b }
 }
