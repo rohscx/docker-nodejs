0b49da225b08693e60b335cf847c237e0430dfda
rohscx <R_E__g_@Macintosh.local>
2017-07-19 14:19:27 -0400

    setup

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -4,27 +4,26 @@ var formData = require('form-data');
 
 
 module.exports = class Rest {
-  POST() { 
-          var formData = {
-  // Pass a simple key-value pair 
-  username: 'devnetuser',
-  password: 'Cisco123'
-};
-request.post({url:'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', formData: formData}, function optionalCallback(err, httpResponse, body) {
-  if (err) {
-    return console.error('upload failed:', err);
+  POST(){
+    var formData = {
+      // Pass a simple key-value pair
+      username: 'devnetuser',
+      password: 'Cisco123'
+    };
+    request.post({url:'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', formData: formData}, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        return console.error('upload failed:', err);
+      }
+      console.log('Upload successful!  Server responded with:', body);
+    });
+    return (
+      {anotherTestResponse: "Cats on everything from API REst POST"}
+    )
   }
-  console.log('Upload successful!  Server responded with:', body);
-});
-      return a + b 
+
+  GET(){
+    return (
+      {anotherTestResponse: "Cats on everything from API REst GET"}
+    )
   }
-  substract(a,b) { return a - b }
-}
-
-    GET() {
-        return (
-    {anotherTestResponse: "Cats on everything from API REst GET"}
-  );
-    }
-
 }
--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -1,9 +1,9 @@
 var rest = require('./api/rest');
 module.exports = () => {
-  var newRest = rest.httpPOST();
-  var newRest2 = rest.httpGET();
-  var output = newRest.anotherTestResponse;
-  var output2 = newRest2.anotherTestResponse;
+  var newRest = rest.POST();
+  var newRest2 = rest.GET();
+  var output = newRest;
+  var output2 = newRest2;
   return (
     {
       testResponse: "Cats on everything from Apic Ticket",
