8cb06bdf6e0696cc78a7312e8480ea44a29bcf0c
rohscx <R_E__g_@Macintosh.local>
2017-07-19 12:38:18 -0400

    setup

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -1,4 +1,16 @@
+var HTTP = require('HTTP');
 module.exports = () => {
+  makeRequest(type, url, options) {
+    try {
+      const result = HTTP.call(type, url, options);
+      // console.log(result); // debug
+      return result;
+    } catch (e) {
+      // Got a network error, timeout, or HTTP error in the 400 or 500 range.
+      console.log(e) // debug
+      return e;
+    }
+  }
   return (
     {anotherTestResponse: "Cats on everything from API REst"}
   );
