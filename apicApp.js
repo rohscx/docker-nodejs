5be24f33d791001b0ca941dccc44d7b9125f04d6
rohscx <R_E__g_@Macintosh.local>
2017-07-19 18:05:20 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -1,6 +1,11 @@
 var restReqeust = require('./modules/apicTicket');
-
-restReqeust.p.then(function(httpReturn){
+let uri = "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket";
+let method = "POST";
+let body = {
+  "username":"devnetuser",
+  "password":"Cisco123!"
+};
+restReqeust.p(uri,method,body).then(function(httpReturn){
   console.log(httpReturn);
 })
 
--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -1,12 +1,6 @@
 var Rest = require('./api/rest');
 
 class restReqeust extends Rest {
-  let uri = "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket";
-  let method = "POST";
-  let body = {
-    "username":"devnetuser",
-    "password":"Cisco123!"
-  };
 
   p = new Rest(uri,method,body);
 }
