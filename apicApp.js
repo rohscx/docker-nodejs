d3710a9cb0f9df79b12db6cde3eed7022ecb6521
rohscx <rohscx@gmail.com>
2017-07-19 17:06:31 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -2,7 +2,7 @@ var restReqeust = require('./modules/apicTicket');
 
 async function main() {
   var quote = await restReqeust.POST();
-  console.log(quote);
+  console.log("BALKAFJLA",quote);
 }
 
   
