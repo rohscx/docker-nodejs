62b273861c94b0dc15fb68084ea52dc56c122d14
rohscx <rohscx@gmail.com>
2017-07-19 15:23:58 -0400

    Update rest.js

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -1,30 +1,7 @@
 var request = require('request');
 var formData = require('form-data');
 
-
-
-module.exports = class rest {
-
-  POST(){
-    var formData = {
-      // Pass a simple key-value pair
-      username: 'devnetuser',
-      password: 'Cisco123'
-    };
-    request.post({url:'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', formData: formData}, function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        return console.error('upload failed:', err);
-      }
-      console.log('Upload successful!  Server responded with:', body);
-    });
-    return (
-      {anotherTestResponse: "Cats on everything from API REst POST"}
-    )
-  }
-
-  GET(){
-    return (
-      {anotherTestResponse: "Cats on everything from API REst GET"}
-    )
-  }
+module.exports = class Calculator {
+  add(a,b) { return a + b }
+  substract(a,b) { return a - b }
 }
