8550001f1facea5f332d85d28b111b9c3444cefa
rohscx <R_E__g_@Macintosh.local>
2017-07-19 11:43:25 -0400

    setup

--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -1,6 +1,8 @@
-//var rest = require('./api/rest');
+var rest = require('./api/rest');
 module.exports = () => {
+  var newRest = rest();
   return (
-    {testResponse: "Cats on everything from Apic Ticket"}
+    {testResponse: "Cats on everything from Apic Ticket"};
+    newRest.anotherTestResponse;
   );
 }
