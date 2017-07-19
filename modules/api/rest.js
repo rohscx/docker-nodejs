b0347707216265d8119f906361d942c91de0b29a
rohscx <rohscx@gmail.com>
2017-07-19 13:31:19 -0400

    Update rest.js

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -1,25 +1,28 @@
 var httpRequest = require("http-request")
 var http = require("http")
 module.exports = () => {
-// multipart/form-data request built with form-data (3rd party module)
-// you have to use the http export of FormData
-// the instanceof operator fails otherwise
-// implementing a manual check is not very elegant
-var form = new http.FormData();
+var reqBody = {
+	param1: 'value1',
+	param2: 'value2'
+};
 
-form.append('username', 'devnetuser');
-form.append('password', 'Cisco123');
+// this serialization also does URL encoding so you won't have to
+reqBody = querystring.stringify(reqBody);
 
 http.post({
-	url: 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket',
-	reqBody: form
-}, function (err, res) {
+	url: 'http://localhost/post',
+	reqBody: new Buffer(reqBody),
+	headers: {
+		// specify how to handle the request, http-request makes no assumptions
+		'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+	}
+}, 'post.bin', function (err, res) {
 	if (err) {
 		console.error(err);
 		return;
 	}
 	
-	console.log(res.code, res.headers, res.buffer.toString());
+	console.log(res.code, res.headers, res.file);
 });
   return (
     {anotherTestResponse: "Cats on everything from API REst"}
