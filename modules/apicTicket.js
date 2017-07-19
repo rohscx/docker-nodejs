20bb340ab6ef5d0ceb4ef21be8bba547c66666f8
rohscx <R_E__g_@Macintosh.local>
2017-07-19 11:47:01 -0400

    setup

--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -1,9 +1,9 @@
 var rest = require('./api/rest');
 module.exports = () => {
   var newRest = rest();
-  var output = newRest.anotherTestResponse
+  var output = newRest.anotherTestResponse;
+  console.log(output);
   return (
     {testResponse: "Cats on everything from Apic Ticket"}
-    output
   );
 }
