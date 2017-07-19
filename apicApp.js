d7b4d00a679683d90ffce0865c66c26bd57934cc
rohscx <R_E__g_@Macintosh.local>
2017-07-19 18:31:54 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -7,9 +7,9 @@ let body = {
   "username":"devnetuser",
   "password":"Cisco123!"
 };
-restReqeust.Test(method,uri,body).then(function(httpReturn){
+restReqeust.POST(method,uri,body).then(function(httpReturn){
   console.log(httpReturn);
 })
 
 
-console.log(restReqeust.multiply(2,5)); // REST POST
+//console.log(test) // REST POST
--- modules/apicTicket.js
+++ modules/apicTicket.js
@@ -4,12 +4,6 @@ class restReqeust extends Rest {
 
   multiply(a,b) { return a * b }
   divde(a,b) { return a / b }
-  Test (a,b,c) {
-    (a,b,c) => {
-      new Rest(a,b,c)
-    }
-  }
-
 }
 
 
