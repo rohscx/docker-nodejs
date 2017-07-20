daeedc735197868ec858c0e9650c350dd24cc6cb
rohscx <R_E__g_@Macintosh.local>
2017-07-20 16:29:19 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -1,5 +1,6 @@
 const apicTicket = require('./modules/apicTicket');
 const apicDevices = require('./modules/apicDevices');
+const apicConfig = require('./modules/apicConfig');
 
 // Apic Ticket debug
 apicTicket.debug()
@@ -13,6 +14,13 @@ apicTicket.httpRequest()
   })
   .then((devicesReturn) =>{
     console.log(devicesReturn);
+    apicConfig.setHeaders(apicTicket.getTicketData();
+    apicConfig.setDeviceId("4af8bf34-295f-46f4-97b7-0a2d2ea4cf22");
+    apicConfig.setUri();
+    return apicConfig.httpRequest();
+  })
+  .then((configReturn) =>{
+    console.log(configReturn);
   })
   .catch((httpReject) =>{
     console.log(JSON.stringify(httpReject));
---
+++ modules/apicConfig.js
@@ -0,0 +1,36 @@
+const rest = require('./api/rest');
+const securityFile = require('../securityFile')
+
+
+const method = 'GET';
+const uri = securityFile.public.apicEM.uri+"/network-device";
+const headers = "";
+const body = "";
+
+class apicConfig extends rest {
+  constructor (method,uri,headers,body){
+    super(method,uri,headers,body)
+    this.deviceId = "";
+  }
+
+  setHeaders(ticket){
+    this.headers = {
+        "content-type": "application/json",
+        "x-auth-token": ticket,
+      };
+
+  setDeviceId(deviceId){
+    this.deviceId = "/"+deviceId;
+  }
+
+  setUri(){
+    this.uri = this.uri+this.deviceId+"/config";
+  }
+
+  // Adds a Debugs for the contest of the Ticket POST HTTP request
+  debug() {
+    console.log("method: "+this.method,'\n',"uri: "+this.uri,'\n',"headers: "+JSON.stringify(this.headers),'\n',"body: "+JSON.stringify(this.body))
+  }
+}
+
+module.exports = new apicConfig(method,uri,headers,body)
