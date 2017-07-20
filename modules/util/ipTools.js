b02b0ee2d84e68f7e6c74e9ce587df15101f01e6
rohscx <R_E__g_@Macintosh.local>
2017-07-20 18:17:45 -0400

    setup

--- modules/util/ipTools.js
+++ modules/util/ipTools.js
@@ -13,7 +13,7 @@ module.exports = {
     console.log()
   },
   readFile: () =>{
-    return fs.readfileSync("../serverData/ipList.csv").toString();
+    return fs.readFileSync("../serverData/ipList.csv").toString();
   }
 
 }
