4c346f659a5899fde95b34211a485ab66b3bf1b4
rohscx <R_E__g_@Macintosh.local>
2017-07-20 13:31:21 -0400

    setup

--- modules/util/ipTools.js
+++ modules/util/ipTools.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFileSync("../serverData/test.csv", "Test,Test");
-console.log(fs.readfileSync("../serverData/test.csv").toString());
 
 //path.normalize(): example /opt/datafiles/about.html
 //path.dirname();  example /opt/datafiles
 //path.basename(); example about.html
 //path.extname(); example .html
+
+fs.writeFileSync("../serverData/test.csv", "Test,Test");
+
+dataFile = fs.readfileSync("../serverData/test.csv").toString();
+
+console.log(fs.readfileSync("../serverData/test.csv").toString());
