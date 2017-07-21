08dfdb7bde04d39ae8d47673634f72d58073ed25
rohscx <R_E__g_@Macintosh.local>
2017-07-21 12:03:30 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -49,11 +49,10 @@ apicTicket.httpRequest()
         })
         resolve("completed")
       })
-
-    })
-    .then((devicesReturn) =>{
-      console.log(apicDiscovery.getDiscoveryTickets());
-      console.log("asdfasdfasdfasdfasfasdfasdfasdasdfadsfasdfasdfadsfasdfdsafadsfasdfadfafasfsadfasdf")
+      .then((devicesReturn) =>{
+        console.log(apicDiscovery.getDiscoveryTickets());
+        console.log("asdfasdfasdfasdfasfasdfasdfasdasdfadsfasdfasdfadsfasdfdsafadsfasdfadfafasfsadfasdf")
+      })
     })
   })
   // Catches any errors from the HTTP Rest Request
