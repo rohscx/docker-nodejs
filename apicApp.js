d4aed095d47f52b0e1df8fb2c1bf0ecc128a2481
rohscx <R_E__g_@Macintosh.local>
2017-07-21 19:06:13 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -16,7 +16,7 @@ const ipTools = require('./modules/ipTools');
 // Reads the value of a properly formated CSV file, processes it, and stores it.
 ipTools.setFile("ipList.csv")
 ipTools.readFile()
-ipTools.formatData(ipList,20);
+ipTools.formatData(20);
 console.log(ipRangeArray);
 
 
