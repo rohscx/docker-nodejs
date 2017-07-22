e8a721262bcf973440cd6c55d263fcbb7fe7fbda
rohscx <R_E__g_@Macintosh.local>
2017-07-22 13:40:18 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -23,6 +23,7 @@ class ipTools extends fileSystem {
       console.log("BOOOOOOOOOOLAN",newData.filter(Boolean))
       newData.map((data) => {
         console.log(data, "<=Array")
+        console.log(data.trip(), "<=Trrrrm")
         cleanData.push(data.trim())
       })
       if (cleanData){
