b9289911712a7b7cf15eb2af05bf04634bda0c88
rohscx <R_E__g_@Macintosh.local>
2017-07-22 11:45:33 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -18,7 +18,7 @@ class ipTools extends fileSystem {
 
   cleanData (){
     return new Promise((resolve, reject) =>{
-      let newArray= [this.fileData];
+      let newArray= this.fileData;
       let test = this.fileData.split("\r")
       let newData = this.fileData.split("\n");
       //
