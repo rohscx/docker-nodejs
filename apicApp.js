da6956c9a425b65ae1e51f9544052031b58f9fc2
rohscx <R_E__g_@Macintosh.local>
2017-07-22 11:56:03 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -32,7 +32,7 @@ ipTools.readFile()
 //.then((readReturn) => {
 //  console.log(ipTools.cleanData())
 //})
-Promise.all([ipTools.readFile(),ipTools.cleanData()])
+Promise.all([ipTools.setFile("ipList.csv")])
 .then((dataRe)=>{
   console.log(dataRe);
 })
