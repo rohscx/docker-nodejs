62d6f174d6781fe0a61b13f0ac6ab6a65cd17839
rohscx <R_E__g_@Macintosh.local>
2017-07-23 12:02:19 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -50,7 +50,7 @@ class ipTools extends fileSystem {
             console.log(last[0].length,last[1].length,last[2].length,last[3].length)
           }
           console.log(typeof('first[0]'),typeof('first[1]'),typeof('first[2]'),typeof('first[3]'))
-          //console.log(first[0].length,first[1].length,first[2].length,first[3].length)
+          console.log(first[0].length,first[1].length,first[2].length,first[3].length)
           console.log("TEST MATCH", last == first)
           last = nextPredict;
         }
