861be6f95ba0491020b7655ad9187be991ff46a6
rohscx <R_E__g_@Macintosh.local>
2017-07-23 13:20:28 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -34,8 +34,9 @@ class ipTools extends fileSystem {
           let index;
           for (index = 0; index < data.length; ++index) {
             console.log("char ip1 " + index + ": " + data.charCodeAt(index));
-            if (data.charCodeAt(index) == 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57)
-            temp1 += data.toString().trim()
+            let charCode = ata.charCodeAt(index);
+            if (charCode == 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57)
+            temp1 += fromCharCode(charCode).toString().trim()
           }
         })
         return temp1;
