a6b79c1a7f4afb9e31ae8213a1912763cfec1f2b
rohscx <R_E__g_@Macintosh.local>
2017-07-22 20:37:12 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -22,7 +22,7 @@ class ipTools extends fileSystem {
       let last = "";
       let nextPredict= "";
       let nextP = (binary) => {
-        console.log(typeOf("binary"))
+        console.log(typeof("binary"))
         console.log("IN>>> ",binary)
         let baseTen = parseInt(binary, 10);
         console.log("IN>>> ",baseTen)
