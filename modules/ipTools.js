94b9f7d05bd15f58093ae4bfcc1bb2c47b32f176
rohscx <R_E__g_@Macintosh.local>
2017-07-23 13:25:39 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -36,7 +36,8 @@ class ipTools extends fileSystem {
 
             let charCode = data.charCodeAt(index);
             if (charCode == 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57)
-            console.log("char ip1 " + index + ": " + data.charCodeAt(index));
+            console.log("char syntheticIp " + index + ": " + data.charCodeAt(index));
+            console.log("hit ", charCode)
             temp1 += String.fromCharCode(charCode).toString().trim()
           }
         })
