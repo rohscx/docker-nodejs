e6b0fe2d03c0cd59fda5ccc7e63bea4b03d3d37a
rohscx <R_E__g_@Macintosh.local>
2017-07-23 14:15:44 -0400

    setup

--- modules/ipTools.js
+++ modules/ipTools.js
@@ -54,7 +54,7 @@ class ipTools extends fileSystem {
         firstIp = data;
         nextPredict = [data[0],data[1],data[2],nextP(data[3])]
         if (lastIp){
-          console.log (" FIRST_IP==   ",firstIp," 1PREDICTION==   ",nextPredict)
+
           console.log (" 1LAST_IP==   ",lastIp," 1PREDICTION==   ",nextPredict)
           console.log(typeof('lastIp[0]'),typeof('lastIp[1]'),typeof('lastIp[2]'),typeof('lastIp[3]'))
           console.log(lastIp[0].length,lastIp[1].length,lastIp[2].length,lastIp[3].length)
@@ -67,6 +67,7 @@ class ipTools extends fileSystem {
 
           }
         } else {
+          console.log (" FIRST_IP==   ",firstIp," 1PREDICTION==   ",nextPredict)
           lastIp = firstIp;
         }
       })
