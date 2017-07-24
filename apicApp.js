8a55edec717cd5b93b8380fbbae06ab5cd4c2235
rohscx <rohscx@gmail.com>
2017-07-24 09:06:16 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -15,6 +15,7 @@ program
   .option('-P, --pineapple', 'Add pineapple')
   .option('-b, --bbq-sauce', 'Add bbq sauce')
   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .options('-r --Reachability', 'Add apiccReachability', apiccReachability)
   .parse(process.argv);
  
 console.log('you ordered a pizza with:');
