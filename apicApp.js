04d19b8608dfa61e3a21ac8c96a2100a55906d24
rohscx <rohscx@gmail.com>
2017-07-24 09:22:45 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -229,14 +229,6 @@ program
   .option('-o, --optional [value]', 'An optional value')
   .option('-c, --collect [value]', 'A repeatable value', collect, [])
   .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
+  .option('-r, --reachability', 'A value that can be increased', apiccReachability)
   .parse(process.argv);
  
-console.log(' int: %j', program.integer);
-console.log(' float: %j', program.float);
-console.log(' optional: %j', program.optional);
-program.range = program.range || [];
-console.log(' range: %j..%j', program.range[0], program.range[1]);
-console.log(' list: %j', program.list);
-console.log(' collect: %j', program.collect);
-console.log(' verbosity: %j', program.verbose);
-console.log(' args: %j', program.args);
