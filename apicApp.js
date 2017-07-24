f5cd9e642ca044cc7d93978407b38feb9075e9f3
rohscx <R_E__g_@Macintosh.local>
2017-07-23 22:51:00 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -7,6 +7,19 @@ const ipTools = require('./modules/ipTools');
 const rl = require('readline');
 const files = require('./modules/files');
 
+var chalk       = require('chalk');
+var clear       = require('clear');
+var CLI         = require('clui');
+var figlet      = require('figlet');
+var inquirer    = require('inquirer');
+var Preferences = require('preferences');
+var Spinner     = CLI.Spinner;
+var GitHubApi   = require('github');
+var _           = require('lodash');
+var git         = require('simple-git')();
+var touch       = require('touch');
+var fs          = require('fs');
+
 
 
 
