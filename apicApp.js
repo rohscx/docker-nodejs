b6013f60911af00755c5308a0c3579b36e75b998
rohscx <rohscx@gmail.com>
2017-07-24 13:04:04 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -243,7 +243,7 @@ program
   .parse(process.argv);
  
 console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
+if (program.peppers) console.log(apiccReachability());
 if (program.pineapple) console.log('  - pineapple');
 if (program.bbqSauce) console.log('  - bbq');
 console.log('  - %s cheese', program.cheese);
