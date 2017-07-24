c0e52bfde2c6b994d20608d45b721f07ebd9b2f2
rohscx <rohscx@gmail.com>
2017-07-24 11:25:26 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -220,8 +220,11 @@ program
   });
  
 program.parse(process.argv);
- 
-if (typeof cmdValue === 'undefined') {
+
+if (program.apicDevices) console.log(apiccDevices());
+if (program.apicDiscovery) console.log(apiccDiscovery(cmdValue, envValue));
+if (program.apicReachability) console.log(apiccReachability());
+if (typeof cmdValue === 'undefined' && program.apicDiscovery) {
    console.error('no command given!');
    process.exit(1);
 }
