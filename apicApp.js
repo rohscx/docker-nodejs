3403b313461f1267fee09b36d5b17a7eaa643cba
rohscx <R_E__g_@Macintosh.local>
2017-07-23 23:08:20 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -32,6 +32,36 @@ console.log(
 );
 
 
+function getGithubCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
 
 getGithubCredentials(function(credentials) {
   var status = new Spinner('Authenticating you, please wait...');
