4a3ef63b8b8a42229314a345514351d6cc84bdff
rohscx <R_E__g_@Macintosh.local>
2017-07-25 10:15:04 -0400

    setup

---
+++ securityMaker.js
@@ -0,0 +1,32 @@
+
+const securityFile = require('./modules/securityFile')
+
+const rl = require('readline');
+const program = require('commander');
+
+
+/* These can be broken up into speperate .js files to accomplish a task
+   ultimately some user interaction front end should be build, but this is a
+   quick ad dirty....
+   Note to self 240 characters can hold about 8 ip RANGES. <-- Batch Maximum!!
+*/
+
+let makeSecurityFile = () => {
+  Promise.all([securityFile.setSaveExtentions(".js"),securityFile.writeFile("securityFile",securityFile.getSecurityFile)])
+  .then((fileReturn) => {
+    console.log(fileReturn)
+  })
+  .catch((httpReject) =>{
+    console.log(httpReject);
+  })
+}
+
+
+program
+  .version('0.1.0')
+  .usage('<keywords>')
+  .option('-S, --securityFile', 'makeSecurityFile')
+  .parse(process.argv);
+
+if(!program.args.length) program.help();
+console.log(program.args);
