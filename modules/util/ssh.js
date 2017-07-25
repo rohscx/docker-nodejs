af9111a51383eed771a5904964f1a4ce045f9e2f
rohscx <rohscx@gmail.com>
2017-07-25 19:18:54 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -77,3 +77,23 @@ module.exports = class ssh {
     SSH.connect();
   }
 }
+//host configuration with connection settings and commands 
+var host = {
+ server:        {     
+  host:         "127.0.0.1",
+  userName:     "test",
+  password:     "1234",
+ },
+ commands:      [ "echo $(pwd)", "ls -l" ]
+};
+ 
+var SSH2Shell = require ('ssh2shell'),
+  //Create a new instance passing in the host object 
+  SSH = new SSH2Shell(host),
+  //Use a callback function to process the full session text 
+  callback = function(sessionText){
+    console.log(sessionText)
+  }
+ 
+//Start the process 
+SSH.connect(callback);
