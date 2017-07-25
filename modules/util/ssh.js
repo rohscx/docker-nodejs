95a1c2538d97bebb090df15c782944889f3be88d
rohscx <R_E__g_@Macintosh.local>
2017-07-25 17:40:42 -0400

    setup

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -1,7 +1,7 @@
 var SSH = require('simple-ssh');
 
 module.exports = class ssh {
-  constructor (host,userName,passWord){
+  constructor (username,password,host){
     this.host = host;
     this.userName = userName;
     this.passWord = passWord;
