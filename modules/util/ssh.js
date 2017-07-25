ef3d54dc9ee92817a067c37e70e2a3fa9f053c0b
rohscx <rohscx@gmail.com>
2017-07-25 18:35:31 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -38,7 +38,7 @@ conn.on('ready', function() {
   password: this.password,
   algorithms: {
     algorithms: {
-    serverHostKey: [ 'ssh-rsa', 'ssh-dss','aes256-cbc','hmac-sha1-160']
+    serverHostKey: [ 'ssh-rsa', 'ssh-dss','aes256-cbc','hmac-sha1-160','esp-3des', 'esp-md5-hmac']
   }
 }
 });
