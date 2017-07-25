adc1c3791c3d0b3505dc33f91af6739bf78b9f3f
rohscx <rohscx@gmail.com>
2017-07-25 15:51:30 -0400

    Update apicReachability.js

--- modules/apicReachability.js
+++ modules/apicReachability.js
@@ -35,7 +35,7 @@ class apicReachability extends rest {
     let returnData = this.returnData.response;
     returnData.map((data,index) => {
       for (let [key, value] of Object.entries(data)) {
-        if (value.toLowerCase() == "unreachable" && counter == 0) {
+        if (value.toLowerCase() == "unreachable") {
           console.log("Key VAL  ",key," ", value,"  ",returnData[index] )
           this.unReachable.push("Key VAL  ",key," ", value,"  ",returnData[index])
         } else {
