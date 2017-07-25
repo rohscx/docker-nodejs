1871e40847f4d72651c3d8d6dbe840e3e3f2f913
rohscx <rohscx@gmail.com>
2017-07-25 15:02:20 -0400

    Update apicApp.js

--- apicApp.js
+++ apicApp.js
@@ -192,49 +192,6 @@ let apiccDiscovery = (inputFile,jobName) => {
 
 
 
-/*
-// Gets an ApicEM Ticket
-// Apic Ticket debug
-apicTicket.debug()
-apicTicket.httpRequest()
-  .then((ticketReturn) =>{
-    console.log(ticketReturn);
-    apicTicket.setTicketData(ticketReturn.response);
-    apicDevices.setHeaders(apicTicket.getTicketData());
-    apicDevices.setUriBase(apicTicket.getUriBase());
-    // Uses ticket to pull device list
-    return apicDevices.httpRequest();
-  })
-  .then((devicesReturn) =>{
-    console.log(devicesReturn);
-    apicConfig.setHeaders(apicTicket.getTicketData());
-    apicConfig.setUriBase(apicTicket.getUriBase());
-    apicConfig.setDeviceId("4af8bf34-295f-46f4-97b7-0a2d2ea4cf22");
-    apicConfig.setUri();
-    // Uses ticket to pull a single device config
-    return apicConfig.httpRequest();
-  })
-  .then((configReturn) =>{
-    // prints out stringified JSON
-    console.log(JSON.stringify(configReturn));
-    // prints out pretty string
-    console.log(configReturn.response.toString())
-    apicReachability.setHeaders(apicTicket.getTicketData());
-    apicReachability.setUriBase(apicTicket.getUriBase());
-    // Uses ticket to pull device reachablidy information
-    return apicReachability.httpRequest();
-  })
-  .then((reachabilityReturn) =>{
-    console.log(reachabilityReturn);
-  })
-  // Catches any errors from the HTTP Rest Request
-  .catch((httpReject) =>{
-    console.log(httpReject);
-  })
-*/
-
-
-
 let appMenu = () => {
   program
     .version('0.1.0')
