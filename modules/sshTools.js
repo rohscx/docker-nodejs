e8636719af9356d61efcbb9f5c7bf1c542d31bdb
rohscx <rohscx@gmail.com>
2017-07-25 21:11:44 -0400

    Update sshTools.js

--- modules/sshTools.js
+++ modules/sshTools.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const fileSystem = require('./util/ssh')
+const fileSystem = require('./util/ciscoCli')
 const securityFile = require('../securityFile')
 //path.normalize(): example /opt/datafiles/about.html
 //path.dirname();  example /opt/datafiles
@@ -21,26 +21,52 @@ const password = privateSsh.password ? privateSsh.password : "Please Set uPass!!
 const host = privateSsh.host ? "localhost" : "localhost";
 
 
-class sshTools extends fileSystem {
-  constructor (username,password,host){
-    super(username,password,host)
-  }
-
-  setHost(data){
-    this.host = data;
-  }
-
-  setUname(data){
-    this.username = data;
-  }
-
-  setUpass(data){
-    this.password = data;
-  }
-  // Adds a Debugs for the contest of the Ticket POST HTTP request
-  debug() {
-    console.log("username: "+this.username,'\n',"password: "+this.password,'\n',"host: "+this.host,'\n')
-  }
+module.exports = class sshTools {
+var Expectly = require('expectly').Expectly;
+var Patterns = require('expectly').Patterns;
+ 
+var settings = {
+    host: 'switch1',
+    port: 22,
+    protocol: 'ssh',
+    username: 'cisco',
+    password: 'cisco',
+    autoEnable: true,
+    enablePassword: 'cisco'
 }
-
-module.exports = new sshTools(username,password,host)
+ 
+// These regexs are used to grab the content between the begin and start of a config.
+var START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
+var END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
+ 
+ 
+var expectly = new Expectly(settings);
+ 
+// Show errors...
+expectly.on('error', function(err){
+    console.log('Error', err)
+});
+ 
+expectly.on('ready', function(session){
+ 
+    // At this point you should be logged in. The session object is an expectly-stream session.
+ 
+    // Here we execute a 'show run' command to see 
+ 
+    session.sync()
+        .send("show run\n")
+        .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
+            // Store the configuration in the sessions config variable.
+            session.set('config', results);
+            done();
+        })
+        .end(function(err){
+            // Get the configuration from the sessions config variable	
+            var deviceConfig = session.get('config');
+            console.log(deviceConfig);
+            expectly.end();
+    })
+})
+ 
+expectly.connect();
+}
