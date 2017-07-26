129e64d1980bf1c5697d036679e077d557f67930
rohscx <rohscx@gmail.com>
2017-07-26 10:31:24 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -1,6 +1,16 @@
 const SSH2Shell = require ('ssh2shell')
 const SSH = require('simple-ssh');
 
+module.exports = class ssh {
+  constructor (username,password,host){
+    this.host = host;
+    this.username = username;
+    this.password = password;
+    this.port = 22;
+  }
+
+  makeCon(){
+    const SSH2Shell = require ('ssh2shell')
 
 module.exports = class ssh {
   constructor (username,password,host){
@@ -14,33 +24,15 @@ module.exports = class ssh {
    var ssh = new SSH({
     host: this.host,
     user: this.username,
-    pass: this.password,
-     algorithms: {
-           kex: [
-              'diffie-hellman-group1-sha1',
-              'ecdh-sha2-nistp256',
-              'ecdh-sha2-nistp384',
-              'ecdh-sha2-nistp521',
-              'diffie-hellman-group-exchange-sha256',
-              'diffie-hellman-group14-sha1'],
-           cipher: [
-              'aes128-ctr',
-              'aes192-ctr',
-              'aes256-ctr',
-              'aes128-gcm',
-              'aes128-gcm@openssh.com',
-              'aes256-gcm',
-              'aes256-gcm@openssh.com',
-              'aes256-cbc' ]
-           }
+    pass: this.password
 });
-
+ 
 ssh.exec('echo $PATH', {
     out: function(stdout) {
         console.log(stdout);
     }
 }).start();
-
+ 
 /*** Using the `args` options instead ***/
 ssh.exec('echo', {
     args: ['$PATH'],
