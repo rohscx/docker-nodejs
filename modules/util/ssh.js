ddd6bef3f01d810a3c34aef806c5d7db9a1940d5
rohscx <rohscx@gmail.com>
2017-07-26 10:39:55 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -1,5 +1,9 @@
-const SSH2Shell = require ('ssh2shell')
-const SSH = require('simple-ssh');
+      var path, node_ssh, ssh, fs
+ 
+const fs = require('fs')
+const path = require('path')
+const node_ssh = require('node-ssh')
+
 
 module.exports = class ssh {
   constructor (username,password,host){
@@ -10,19 +14,68 @@ module.exports = class ssh {
   }
 
   makeCon(){
-   var ssh = new SSH({
-    host: this.host,
-    user: this.username,
-    pass: this.password,
-     key: "diffie-hellman-group-exchange-sha256"
-});
- 
-ssh.exec('echo $PATH', {
-    out: function(stdout) {
-        console.log(stdout);
+
+ let ssh = new node_ssh()
+ssh.connect({
+  host: this.host,
+  username: this.username,
+  password: this.password
+})
+
+.then(function() {
+  // Local, Remote 
+  ssh.putFile('/home/steel/Lab/localPath', '/home/steel/Lab/remotePath').then(function() {
+    console.log("The File thing is done")
+  }, function(error) {
+    console.log("Something's wrong")
+    console.log(error)
+  })
+  // Array<Shape('local' => string, 'remote' => string)> 
+  ssh.putFiles([{ local: '/home/steel/Lab/localPath', remote: '/home/steel/Lab/remotePath' }]).then(function() {
+    console.log("The File thing is done")
+  }, function(error) {
+    console.log("Something's wrong")
+    console.log(error)
+  })
+  // Local, Remote 
+  ssh.getFile('/home/steel/Lab/localPath', '/home/steel/Lab/remotePath').then(function(Contents) {
+    console.log("The File's contents were successfully downloaded")
+  }, function(error) {
+    console.log("Something's wrong")
+    console.log(error)
+  })
+  // Putting entire directories 
+  const failed = []
+  const successful = []
+  ssh.putDirectory('/home/steel/Lab', '/home/steel/Lab', {
+    recursive: true,
+    validate: function(itemPath) {
+      const baseName = path.basename(itemPath)
+      return baseName.substr(0, 1) !== '.' && // do not allow dot files 
+             baseName !== 'node_modules' // do not allow node_modules 
+    },
+    tick: function(localPath, remotePath, error) {
+      if (error) {
+        failed.push(localPath)
+      } else {
+        successful.push(localPath)
+      }
     }
-}).start();
- 
-}
+  }).then(function(status) {
+    console.log('the directory transfer was', status ? 'successful' : 'unsuccessful')
+    console.log('failed transfers', failed.join(', '))
+    console.log('successful transfers', successful.join(', '))
+  })
+  // Command 
+  ssh.execCommand('hh_client --json', { cwd:'/var/www' }).then(function(result) {
+    console.log('STDOUT: ' + result.stdout)
+    console.log('STDERR: ' + result.stderr)
+  })
+  // Command with escaped params 
+  ssh.exec('hh_client', ['--json'], { cwd: '/var/www', stream: 'stdout', options: { pty: true } }).then(function(result) {
+    console.log('STDOUT: ' + result)
+  })
+})
+  }
 
 }
