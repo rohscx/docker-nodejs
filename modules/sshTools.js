c5fe8c5a95d652ee106a33ebd083097577eb9764
rohscx <R_E__g_@Macintosh.local>
2017-07-25 21:22:03 -0400

    setup

--- modules/sshTools.js
+++ modules/sshTools.js
@@ -22,49 +22,74 @@ const password = privateSsh.password ? privateSsh.password : "Please Set uPass!!
 const host = privateSsh.host ? "localhost" : "localhost";
 
 
-module.exports = class sshTools {
-  let settings = {
-      host: host,
-      port: 22,
-      protocol: 'ssh',
-      username: username,
-      password: password,
-      autoEnable: false,
-      enablePassword: 'cisco'
+module.exports = new class sshTools {
+  constructor (username,password,host){
+  this.host = host;
+  this.username = username;
+  this.password = password;
+  this.port = 22;
   }
 
-  // These regexs are used to grab the content between the begin and start of a config.
-  let START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
-  let END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
+  setHost(data){
+  this.host = data;
+  }
+
+  setUname(data){
+  this.username = data;
+  }
+
+  setUpass(data){
+    this.password = data;
+  }
+  // Adds a Debugs for the contest of the Ticket POST HTTP request
+  debug() {
+    console.log("username: "+this.username,'\n',"password: "+this.password,'\n',"host: "+this.host,'\n')
+  }
+
+  makeCon(){
+    let settings = {
+        host: host,
+        port: 22,
+        protocol: 'ssh',
+        username: username,
+        password: password,
+        autoEnable: false,
+        enablePassword: 'cisco'
+    }
+
+    // These regexs are used to grab the content between the begin and start of a config.
+    let START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
+    let END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
 
 
-  let expectly = new Expectly(settings);
+    let expectly = new Expectly(settings);
 
-  // Show errors...
-  expectly.on('error', function(err){
-      console.log('Error', err)
-  });
+    // Show errors...
+    expectly.on('error', function(err){
+        console.log('Error', err)
+    });
 
-  expectly.on('ready', function(session){
+    expectly.on('ready', function(session){
 
-      // At this point you should be logged in. The session object is an expectly-stream session.
+        // At this point you should be logged in. The session object is an expectly-stream session.
 
-      // Here we execute a 'show run' command to see
+        // Here we execute a 'show run' command to see
 
-      session.sync()
-          .send("show run\n")
-          .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
-              // Store the configuration in the sessions config letiable.
-              session.set('config', results);
-              done();
-          })
-          .end(function(err){
-              // Get the configuration from the sessions config letiable
-              let deviceConfig = session.get('config');
-              console.log(deviceConfig);
-              expectly.end();
-      })
-  })
+        session.sync()
+            .send("show run\n")
+            .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
+                // Store the configuration in the sessions config letiable.
+                session.set('config', results);
+                done();
+            })
+            .end(function(err){
+                // Get the configuration from the sessions config letiable
+                let deviceConfig = session.get('config');
+                console.log(deviceConfig);
+                expectly.end();
+        })
+    })
 
-  expectly.connect();
+    expectly.connect();
+  }
 }
