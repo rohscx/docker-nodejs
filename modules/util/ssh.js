4628a1bcb6e26617489bbd82bb0e1b8a9c3c643c
rohscx <R_E__g_@Macintosh.local>
2017-07-25 22:16:28 -0400

    setup

---
+++ modules/util/ssh.js
@@ -0,0 +1,41 @@
+var SSH = require('simple-ssh');
+
+module.exports = class ssh {
+  constructor (username,password,host){
+    this.host = host;
+    this.username = username;
+    this.password = password;
+    this.port = 22;
+  }
+
+  makeCon(){
+    var Client = require('ssh2').Client;
+
+var conn = new Client();
+conn.on('ready', function() {
+  console.log('Client :: ready');
+  conn.forwardOut('192.168.100.102', 8000, '127.0.0.1', 80, function(err, stream) {
+    if (err) throw err;
+    stream.on('close', function() {
+      console.log('TCP :: CLOSED');
+      conn.end();
+    }).on('data', function(data) {
+      console.log('TCP :: DATA: ' + data);
+    }).end([
+      'HEAD / HTTP/1.1',
+      'User-Agent: curl/7.27.0',
+      'Host: 127.0.0.1',
+      'Accept: */*',
+      'Connection: close',
+      '',
+      ''
+    ].join('\r\n'));
+  });
+}).connect({
+  host: this.host,
+  port: this.port,
+  username: this.username,
+  password: this.password
+});
+}
+}
