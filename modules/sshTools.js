e9dfff74a47ea486eb3025327659505fa8edfbec
rohscx <rohscx@gmail.com>
2017-07-25 22:08:09 -0400

    Update sshTools.js

--- modules/sshTools.js
+++ modules/sshTools.js
@@ -23,74 +23,42 @@ const password = privateSsh.password ? privateSsh.password : "Please Set uPass!!
 const host = privateSsh.host ? "localhost" : "localhost";
 
 
-module.exports = new class sshTools {
-  constructor (username,password,host){
-  this.host = host;
-  this.username = username;
-  this.password = password;
-  this.port = 22;
-  }
-
-  setHost(data){
-  this.host = data;
-  }
-
-  setUname(data){
-  this.username = data;
-  }
-
-  setUpass(data){
-    this.password = data;
-  }
-  // Adds a Debugs for the contest of the Ticket POST HTTP request
-  debug() {
-    console.log("username: "+this.username,'\n',"password: "+this.password,'\n',"host: "+this.host,'\n')
-  }
-
-  makeCon(){
-    let settings = {
-        host: host,
-        port: 22,
-        protocol: 'ssh',
-        username: username,
-        password: password,
-        autoEnable: false,
-        enablePassword: 'cisco'
-    }
-
-    // These regexs are used to grab the content between the begin and start of a config.
-    let START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
-    let END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
-
-
-    let cliscocli = new CiscoCli(settings);
-
-    // Show errors...
-    cliscocli.on('error', function(err){
-        console.log('Error', err)
+var c = new Connection();
+c.on('connect', function() {
+  console.log('Connection :: connect');
+});
+c.on('ready', function() {
+  console.log('Connection :: ready');
+  c.exec('uptime', function(err, stream) {
+    if (err) throw err;
+    stream.on('data', function(data, extended) {
+      console.log((extended === 'stderr' ? 'STDERR: ' : 'STDOUT: ')
+                  + data);
     });
-
-    cliscocli.on('ready', function(session){
-
-        // At this point you should be logged in. The session object is an cliscocli-stream session.
-
-        // Here we execute a 'show run' command to see
-
-        session.sync()
-            .send("show run\n")
-            .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
-                // Store the configuration in the sessions config letiable.
-                session.set('config', results);
-                done();
-            })
-            .end(function(err){
-                // Get the configuration from the sessions config letiable
-                let deviceConfig = session.get('config');
-                console.log(deviceConfig);
-                cliscocli.end();
-        })
-    })
-
-    cliscocli.connect();
-  }
-}
+    stream.on('end', function() {
+      console.log('Stream :: EOF');
+    });
+    stream.on('close', function() {
+      console.log('Stream :: close');
+    });
+    stream.on('exit', function(code, signal) {
+      console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+      c.end();
+    });
+  });
+});
+c.on('error', function(err) {
+  console.log('Connection :: error :: ' + err);
+});
+c.on('end', function() {
+  console.log('Connection :: end');
+});
+c.on('close', function(had_error) {
+  console.log('Connection :: close');
+});
+c.connect({
+  host: '192.168.100.100',
+  port: 22,
+  username: 'frylock',
+  privateKey: require('fs').readFileSync('/here/is/my/key')
+});
