9daf6485b10ca5f4fbb2ba31d97585bd53ac8a79
rohscx <rohscx@gmail.com>
2017-07-26 12:08:44 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -39,7 +39,7 @@ ssh.connect({
 })
 .then(function() {
   // Command 
-  ssh.execCommand(testData[0],testData[1]).then(function(result) {
+  ssh.execCommand(testData[0]).then(function(result) {
     let returnData = [];
     returnData.push(result.stdout)
     returnData = returnData[0].replace(/\r\n|\n/, '')
