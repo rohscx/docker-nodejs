aafd7ebaa26cd61a29ec294370fd8adf43a23b6e
rohscx <R_E__g_@Macintosh.local>
2017-07-26 10:27:14 -0400

    git pull

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -1,14 +1,6 @@
 const SSH2Shell = require ('ssh2shell')
 const SSH = require('simple-ssh');
 
-module.exports = class ssh {
-  constructor (username,password,host){
-    this.host = host;
-    this.username = username;
-    this.password = password;
-    this.port = 22;
-  }
-
 
 module.exports = class ssh {
   constructor (username,password,host){
