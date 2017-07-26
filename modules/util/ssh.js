9202dddf0b1f73d151e964e521a176f71e485718
rohscx <rohscx@gmail.com>
2017-07-26 09:28:52 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -48,11 +48,6 @@ module.exports = class ssh {
         verbose: true,
         debug: true,
         idleTimeOut: 10000,
-        ["keyboard-interactive"]: function(name, instructions, instructionsLang, prompts, finish){
-           console.log('Connection :: keyboard-interactive');
-           console.log(prompts);
-           finish(["<password>"]);
-        },
         onEnd: function( sessionText, sshObj ) {
            sshObj.msg.send("--------- onEnd has ------------");
            sshObj.msg.send(sessionText);
@@ -62,18 +57,6 @@ module.exports = class ssh {
 
   //Create a new instance
   let SSH = new SSH2Shell(host);
-  SSH.on ('keyboard-interactive', function(name, instructions, instructionsLang, prompts, finish){
-     if (this.sshObj.debug) {this.emit('msg', this.sshObj.server.host + ": Keyboard-interactive");}
-     if (this.sshObj.verbose){
-       this.emit('msg', "name: " + name);
-       this.emit('msg', "instructions: " + instructions);
-       var str = JSON.stringify(prompts, null, 4);
-       this.emit('msg', "Prompts object: " + str);
-       console.log("Cats on everything");
-     }
-     //The example presumes only the password is required 
-     finish([this.sshObj.server.password] );
-  });
   //Start the process
   SSH.connect();
   }
