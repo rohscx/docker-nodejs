2fda87cca061eb1db0e367f35b22c6a20e76155b
rohscx <rohscx@gmail.com>
2017-07-26 10:41:17 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -17,7 +17,26 @@ module.exports = class ssh {
 ssh.connect({
   host: this.host,
   username: this.username,
-  password: this.password
+  password: this.password,
+  algorithms: {
+           kex: [
+              'diffie-hellman-group1-sha1',
+              'ecdh-sha2-nistp256',
+              'ecdh-sha2-nistp384',
+              'ecdh-sha2-nistp521',
+              'diffie-hellman-group-exchange-sha256',
+              'diffie-hellman-group14-sha1'],
+           cipher: [
+              'aes128-ctr',
+              'aes192-ctr',
+              'aes256-ctr',
+              'aes128-gcm',
+              'aes128-gcm@openssh.com',
+              'aes256-gcm',
+              'aes256-gcm@openssh.com',
+              'aes256-cbc' ]
+           }
+        }
 })
 
 .then(function() {
