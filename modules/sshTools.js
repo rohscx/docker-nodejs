e557e0d264d71d98c58993a8101a66b3ea7af36b
rohscx <R_E__g_@Macintosh.local>
2017-07-25 21:15:35 -0400

    setup

--- modules/sshTools.js
+++ modules/sshTools.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const fileSystem = require('./util/ciscoCli')
+const fileSystem = require('./util/fileSystem')
 const securityFile = require('../securityFile')
+let ciscoCli = require('ciscoCli');
 //path.normalize(): example /opt/datafiles/about.html
 //path.dirname();  example /opt/datafiles
 //path.basename(); example about.html
@@ -22,51 +23,48 @@ const host = privateSsh.host ? "localhost" : "localhost";
 
 
 module.exports = class sshTools {
-var Expectly = require('expectly').Expectly;
-var Patterns = require('expectly').Patterns;
- 
-var settings = {
-    host: 'switch1',
-    port: 22,
-    protocol: 'ssh',
-    username: 'cisco',
-    password: 'cisco',
-    autoEnable: true,
-    enablePassword: 'cisco'
-}
- 
-// These regexs are used to grab the content between the begin and start of a config.
-var START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
-var END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
- 
- 
-var expectly = new Expectly(settings);
- 
-// Show errors...
-expectly.on('error', function(err){
-    console.log('Error', err)
-});
- 
-expectly.on('ready', function(session){
- 
-    // At this point you should be logged in. The session object is an expectly-stream session.
- 
-    // Here we execute a 'show run' command to see 
- 
-    session.sync()
-        .send("show run\n")
-        .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
-            // Store the configuration in the sessions config variable.
-            session.set('config', results);
-            done();
-        })
-        .end(function(err){
-            // Get the configuration from the sessions config variable	
-            var deviceConfig = session.get('config');
-            console.log(deviceConfig);
-            expectly.end();
-    })
-})
- 
-expectly.connect();
+  let settings = {
+      host: host,
+      port: 22,
+      protocol: 'ssh',
+      username: username,
+      password: password,
+      autoEnable: false,
+      enablePassword: 'cisco'
+  }
+
+  // These regexs are used to grab the content between the begin and start of a config.
+  let START_CONFIG = Patterns['RANGE START RUNNING-CONFIG'];
+  let END_CONFIG = Patterns['RANGE STOP RUNNING-CONFIG'];
+
+
+  let expectly = new Expectly(settings);
+
+  // Show errors...
+  expectly.on('error', function(err){
+      console.log('Error', err)
+  });
+
+  expectly.on('ready', function(session){
+
+      // At this point you should be logged in. The session object is an expectly-stream session.
+
+      // Here we execute a 'show run' command to see
+
+      session.sync()
+          .send("show run\n")
+          .between(BEGIN_CONFIG, END_CONFIG, function(err, results, done) {
+              // Store the configuration in the sessions config letiable.
+              session.set('config', results);
+              done();
+          })
+          .end(function(err){
+              // Get the configuration from the sessions config letiable
+              let deviceConfig = session.get('config');
+              console.log(deviceConfig);
+              expectly.end();
+      })
+  })
+
+  expectly.connect();
 }
