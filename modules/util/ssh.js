1c75c3758b76fb601c1449c3c3d331a03541c17d
rohscx <rohscx@gmail.com>
2017-07-26 12:55:43 -0400

    Update ssh.js

--- modules/util/ssh.js
+++ modules/util/ssh.js
@@ -39,7 +39,7 @@ ssh.connect({
 })
 .then(function() {
   // Command 
-  ssh.execCommand(testData[0],{stdin:testData[1]}).then(function(result) {
+  ssh.execCommand("show access-lists 99").then(function(result) {
     let returnData = [];
     returnData.push(result.stdout)
     returnData = returnData[0].replace(/\r\n|\n/, '')
