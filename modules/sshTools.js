185e15aab2fcec26b3b47c93661a6fbbfe0b055b
rohscx <R_E__g_@Macintosh.local>
2017-07-25 22:14:20 -0400

    setup

--- modules/sshTools.js
+++ modules/sshTools.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const fileSystem = require('./util/fileSystem')
+const fileSystem = require('./util/ssh')
 const securityFile = require('../securityFile')
-const CiscoCli = require('./util/ciscoCli').CiscoCli;
-const Patterns = require('./util/ciscoCli').Patterns;
 //path.normalize(): example /opt/datafiles/about.html
 //path.dirname();  example /opt/datafiles
 //path.basename(); example about.html
@@ -23,42 +21,26 @@ const password = privateSsh.password ? privateSsh.password : "Please Set uPass!!
 const host = privateSsh.host ? "localhost" : "localhost";
 
 
-var c = new Connection();
-c.on('connect', function() {
-  console.log('Connection :: connect');
-});
-c.on('ready', function() {
-  console.log('Connection :: ready');
-  c.exec('uptime', function(err, stream) {
-    if (err) throw err;
-    stream.on('data', function(data, extended) {
-      console.log((extended === 'stderr' ? 'STDERR: ' : 'STDOUT: ')
-                  + data);
-    });
-    stream.on('end', function() {
-      console.log('Stream :: EOF');
-    });
-    stream.on('close', function() {
-      console.log('Stream :: close');
-    });
-    stream.on('exit', function(code, signal) {
-      console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-      c.end();
-    });
-  });
-});
-c.on('error', function(err) {
-  console.log('Connection :: error :: ' + err);
-});
-c.on('end', function() {
-  console.log('Connection :: end');
-});
-c.on('close', function(had_error) {
-  console.log('Connection :: close');
-});
-c.connect({
-  host: '192.168.100.100',
-  port: 22,
-  username: 'frylock',
-  privateKey: require('fs').readFileSync('/here/is/my/key')
-});
+class sshTools extends fileSystem {
+  constructor (username,password,host){
+    super(username,password,host)
+  }
+
+  setHost(data){
+    this.host = data;
+  }
+
+  setUname(data){
+    this.username = data;
+  }
+
+  setUpass(data){
+    this.password = data;
+  }
+  // Adds a Debugs for the contest of the Ticket POST HTTP request
+  debug() {
+    console.log("username: "+this.username,'\n',"password: "+this.password,'\n',"host: "+this.host,'\n')
+  }
+}
+
+module.exports = new sshTools(username,password,host)
