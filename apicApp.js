6350d2e210d7218dc7e1fb207d5bbc0d78bb3152
rohscx <R_E__g_@Macintosh.local>
2017-07-27 11:01:00 -0400

    setup

--- apicApp.js
+++ apicApp.js
@@ -53,7 +53,7 @@ let apiccReachability= () => {
         apicReachability.setUnreachableBrief()
         ipTools.setSaveExtentions(".csv")
         let reachNameBrief = "reachJobBrief-"+Date.now()
-        return ipTools.writeFile(reachNameBrief,JSON.stringify(apicReachability.getUnreachableBrief(), null, 2))
+        return ipTools.writeFile(reachNameBrief,JSON.stringify(apicReachability.getUnreachableBrief().str.replace("\"",""), null, 2))
       })
       .then((writeReturn) =>{
         console.log(writeReturn)
