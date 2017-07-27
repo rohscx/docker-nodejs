3e7f493935d45d90a6d8c6a996d714521e38f2d3
rohscx <R_E__g_@Macintosh.local>
2017-07-27 14:06:56 -0400

    setup

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ let iseTest = () =>{
   })
   .then((iseReturn) =>{
     console.log("JSON  " , iseReturn)
-    console.log("next Page  " , iseReturn.resources[0].resource.link)
+    console.log("next Page  " , iseReturn.resources[0].resource['$'])
   })
   .catch((httpReject) =>{
     console.log(httpReject);
