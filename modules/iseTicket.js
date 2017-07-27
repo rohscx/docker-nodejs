61670d56d1583ab3f0555f2ecb59d35659703c48
rohscx <R_E__g_@Macintosh.local>
2017-07-27 12:42:04 -0400

    setup

--- modules/iseTicket.js
+++ modules/iseTicket.js
@@ -14,8 +14,8 @@ const privateIse = {
 
 baseConvert64 = (uName,uPass) => {
   let data = uName +":"+uPass;
-  console.log(data.toString('base64'))
-  return data.toString('base64')
+  console.log(new Buffer(data).toString('base64'))
+  return new Buffer(data).toString('base64')
 }
 
 let iseHeaders = {
