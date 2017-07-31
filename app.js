009be5e0e868dea2c74d9b85f12cd38a43cce430
rohscx <R_E__g_@Macintosh.local>
2017-07-31 13:29:59 -0400

    setup

--- app.js
+++ app.js
@@ -30,7 +30,14 @@ let iseTest2 = (inputFile) => {
         .then((readData)=>{
           console.log("dataTest",readData)
           let chunkCount = dataTools.getChunks(readData).length
+          let chunks = dataTools.getChunks(readData);
           console.log("ChunkData", chunkCount)
+          chunks.map((data) =>{
+            console.log(data)
+            data.map((data)=>{
+              console.log(data)
+            })
+          })
         })
         .catch((reject) =>{
           console.log(reject);
