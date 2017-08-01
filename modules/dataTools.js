ad51a8874a3a4ca293df762f9c1b19721c24cf33
rohscx <R_E__g_@Macintosh.local>
2017-07-31 20:30:01 -0400

    setup

--- modules/dataTools.js
+++ modules/dataTools.js
@@ -20,7 +20,7 @@ class dataTools extends fileSystem {
   setDelay (delay){
     let maxDelay = delay;
     let counter = 0;
-    setInterval(() =>{ if (maxDelay == counter){clearInterval(); console.log("Interval")} }, 1000)
+    setInterval(() =>{ if (maxDelay == counter){clearInterval(); console.log("Interval")}; counter++ }, 1000)
   }
 
   setCompare (dataSet1,dataSet2){
