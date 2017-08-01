e8e727f50e1d394b8b0958c70a8817456130593e
rohscx <R_E__g_@Macintosh.local>
2017-07-31 20:33:20 -0400

    setup

--- modules/dataTools.js
+++ modules/dataTools.js
@@ -20,7 +20,8 @@ class dataTools extends fileSystem {
   setDelay (delay){
     let maxDelay = delay;
     let counter = 0;
-    let time = setInterval(() =>{ couner++; console.log("Interval") }, 1000)
+    let time = setInterval(() =>{ counter++; console.log("Interval") }, 1000)
+    time()
     if (maxDelay == counter){clearInterval(time); };
   }
 
