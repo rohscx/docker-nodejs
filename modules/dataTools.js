993b53df19629ec9542f9b1670db14759148f0bd
rohscx <R_E__g_@Macintosh.local>
2017-08-02 12:41:03 -0400

    setup

--- modules/dataTools.js
+++ modules/dataTools.js
@@ -88,7 +88,7 @@ class dataTools extends fileSystem {
   getChunks(inputData){
     let toChunk = inputData;
     //console.log("Chunk DATA", toChunk)
-    let chunkMax = Math.ceil(toChunk.length * .01);
+    let chunkMax = Math.ceil(toChunk.length * .03);
     //console.log("CHUNK...",chunk);
     let chunkData = [];
     let chunkArray = [];
