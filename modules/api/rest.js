2601d99770e1e607e191639a3e69c0ce33537e97
rohscx <rohscx@gmail.com>
2017-08-02 12:27:11 -0400

    Update rest.js

--- modules/api/rest.js
+++ modules/api/rest.js
@@ -18,7 +18,7 @@ module.exports = class rest {
   }
 
   httpRequest(){
-    let limiter = new RateLimiter(1, 250);
+    let limiter = new RateLimiter(5, 250);
   
         return new Promise((resolve, reject) =>{
       const options = {
