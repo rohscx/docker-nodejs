a515b9a188d296f06115d1aad28777d6029f816e
rohscx <R_E__g_@Macintosh.local>
2017-08-02 12:56:16 -0400

    setup

--- modules/dataTools.js
+++ modules/dataTools.js
@@ -13,7 +13,7 @@ class dataTools extends fileSystem {
     this.sortedData = "";
     this.ipRange = "";
     this.ipBase2 = "";
-    this.compare = {dataSet1:{},dataSet2:{}}
+    this.compare = {dataSet1:[],dataSet2:{}}
   }
 
   setCompare (dataSet1,dataSet2){
